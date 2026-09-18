b60ef8c8b977879a828ef982b6fb2b5a878c2d91
A. B. M. Mahmudul Hasan <abmmhasan@users.noreply.github.com>
2026-09-18 12:03:31 +0600

    test: enforce hardened PHP runtime bootstrap

--- tests/runtime-php-contract.sh
+++ tests/runtime-php-contract.sh
@@ -16,7 +16,23 @@ assert_file_contains "$file" 'ARG PHP_EXT'
 assert_file_contains "$file" 'ARG PHP_EXT_VERSIONED'
 assert_file_contains "$file" 'ARG UID=1000'
 assert_file_contains "$file" 'ARG GID=1000'
-assert_file_contains "$file" 'php-cli-setup.sh'
+assert_file_contains "$file" 'ARG SCRIPTOMATIC_REF=main'
+assert_file_contains "$file" 'ARG SCRIPTOMATIC_DOWNLOAD_CONNECT_TIMEOUT=10'
+assert_file_contains "$file" 'ARG SCRIPTOMATIC_DOWNLOAD_MAX_TIME=120'
+assert_file_contains "$file" 'ARG SCRIPTOMATIC_DOWNLOAD_RETRIES=3'
+assert_file_contains "$file" 'Scriptomatic/${SCRIPTOMATIC_REF}/bash/php-cli-setup.sh'
+assert_file_contains "$file" '--connect-timeout "${SCRIPTOMATIC_DOWNLOAD_CONNECT_TIMEOUT}"'
+assert_file_contains "$file" '--max-time "${SCRIPTOMATIC_DOWNLOAD_MAX_TIME}"'
+assert_file_contains "$file" '--retry "${SCRIPTOMATIC_DOWNLOAD_RETRIES}"'
+assert_file_contains "$file" 'test -s "$bootstrap"'
+assert_file_contains "$file" 'bash -n "$bootstrap"'
+assert_file_contains "$file" 'SCRIPTOMATIC_REF="${SCRIPTOMATIC_REF}"'
+assert_file_contains "$file" 'bash "$bootstrap" "${USERNAME}" "${PHP_VERSION}"'
 assert_file_contains "$file" 'ENTRYPOINT ["/usr/local/bin/php-entry"]'
 assert_file_contains "$file" 'CMD ["php-fpm"]'
-pass "PHP runtime customization contract"
+
+if grep -Eq '^ADD https?://|Scriptomatic/master/' "$file"; then
+  fail "PHP runtime must not use remote ADD or the stale Scriptomatic master ref"
+fi
+
+pass "PHP selected-version Alpine runtime and Scriptomatic bootstrap contract"
